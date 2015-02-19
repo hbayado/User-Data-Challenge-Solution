16c82df0a4e675bd16f89a001d471191ddef618e
Hazem Bayado <hbayado@gmail.com>
2015-02-19 05:23:50 +0400

    Problem 1 and 2 Solution

---
+++ User Data Challenge Solution/UserData.h
@@ -0,0 +1,13 @@
+//
+//  UserData.h
+//  User Data Challenge Solution
+//
+//  Created by Hazem Bayado on 2/19/15.
+//  Copyright (c) 2015 Hawsaba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserData : NSObject
+
+@end
---
+++ User Data Challenge Solution/UserData.m
@@ -0,0 +1,13 @@
+//
+//  UserData.m
+//  User Data Challenge Solution
+//
+//  Created by Hazem Bayado on 2/19/15.
+//  Copyright (c) 2015 Hawsaba. All rights reserved.
+//
+
+#import "UserData.h"
+
+@implementation UserData
+
+@end
